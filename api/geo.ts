ecf446f9e5a594b40b6a32b76512218bd647259c
karl <karl@localhost>
2026-03-31 12:13:53 +0200

    Enable edge runtime and add audio files

--- api/geo.ts
+++ api/geo.ts
@@ -1,5 +1,7 @@
 import { geolocation } from '@vercel/functions';
 
+export const runtime = 'edge';
+
 export default function handler(request: Request): Response {
   const geo = geolocation(request);
 
